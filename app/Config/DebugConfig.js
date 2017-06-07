4b24a633864a68cc0f3ef1bd9a7ff3884fce2178
Renaud Merle <rndmerle@gmail.com>
2017-06-07 11:47:30 +0200

    Added why-did-you-update React monkey to test renders

--- app/Config/DebugConfig.js
+++ app/Config/DebugConfig.js
@@ -6,11 +6,14 @@ export default {
     'Setting a timer for',
     'Debugger and device times',
     'Unable to symbolicate stack trace',
-    '[...effects]',
+    '[...effects]', // saga ?
+    '%cfont-weight: bold "Value did not change.', // why-did-you-update
+    'Value is a function. Possibly avoidable re-render', // why-did-you-update
   ],
   reduxLogging: __DEV__,
   navigationLogging: false,
+  whyDidYouUpdate: false,
   useReduxDevtoolsExtension: __DEV__,
   useReactotron: false, // adb reverse tcp:9090 tcp:9090
-  notImportantLogs: ['Navigation Dispatch', 'Running application'],
+  notImportantLogs: ['Navigation Dispatch', 'Running application'], // Reactotron
 };
--- app/Config/index.js
+++ app/Config/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import DebugConfig from './DebugConfig';
 import AppConfig from './AppConfig';
 
@@ -5,5 +6,10 @@ export default () => {
   if (__DEV__) {
     console.disableYellowBox = !DebugConfig.yellowBox;
     console.ignoredYellowBox = DebugConfig.ignoredYellowBox;
+
+    if (DebugConfig.whyDidYouUpdate) {
+      const { whyDidYouUpdate } = require('why-did-you-update');
+      whyDidYouUpdate(React, { exclude: /^YellowBox/ });
+    }
   }
 };
