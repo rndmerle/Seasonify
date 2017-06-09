e10741bb00f9d22a91acbf7f4dcca032d2dab813
Renaud Merle <rndmerle@gmail.com>
2017-06-10 01:58:47 +0200

    Reactivated state persistance

--- app/Config/PersistConfig.js
+++ app/Config/PersistConfig.js
@@ -1,12 +1,12 @@
 import { AsyncStorage } from 'react-native';
 
 export default {
-  active: false,
-  reducerVersion: '0.2',
+  active: true,
+  reducerVersion: '0.3',
   storeConfig: {
     storage: AsyncStorage,
     // blacklist: ['login', 'search'], // reducer keys NOT stored to persistence
-    whitelist: ['friends'], // Optionally, just specify the keys you DO want stored to
+    whitelist: ['tvshows', 'friends'], // Optionally, just specify the keys you DO want stored to
     // transforms: [immutablePersistenceTransform]
   },
 };
